aeb90d5df11c936fe51667640f750dd38ea9ed4b
Hunter Winterhalt <hunterwinterhalt@Hunters-MacBook-Air.local>
2016-06-03 15:28:23 -0700

    Add report page with form, successfully pulled alien type for drop down menu.

--- src/app/+reports/reports.component.ts
+++ src/app/+reports/reports.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, Routes, ROUTER_DIRECTIVES , ROUTER_PROVIDERS} from '@angular/router';
+import { AlienService, EncounterService } from  '../shared/services';
+import { IAlien, Encounter } from '../shared/models';
 
 @Component({
   moduleId: module.id,
@@ -7,13 +9,28 @@ import { Router, Routes, ROUTER_DIRECTIVES , ROUTER_PROVIDERS} from '@angular/ro
   templateUrl: 'reports.component.html',
   styleUrls: ['reports.component.css'],
   directives: [ROUTER_DIRECTIVES],
-  providers: [ROUTER_PROVIDERS]
+  providers: [AlienService, EncounterService]
 })
 export class ReportsComponent implements OnInit {
+  
+  public NO_ALIEN_SELECTED: string;
+  public aliens: IAlien[];
+  public encounters: Encounter;
+  public savedColonist: string;
 
-  constructor() {}
-
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private alienService: AlienService,
+    private encounterService: EncounterService
+  ) {
+    this.NO_ALIEN_SELECTED = '(none)';
+    this.savedColonist = sessionStorage.getItem('colonistid');
   }
+
+  ngOnInit() : void {
+    this.alienService.getAliens().then( alientype => this.aliens = alientype );
+    this.encounters = new Encounter(this.NO_ALIEN_SELECTED, '', this.savedColonist, '');
+  }
+  
 
 }
